ef28fe3ea6140b688b2b59aa1260c53a06cc26b3
Doston <dostonberuniy@gmail.com>
2022-02-09 13:29:03 +0500

    material model -m

--- app/Models/Material.php
+++ app/Models/Material.php
@@ -6,29 +6,23 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * Class Product
+ * Class Material
  * @package App\Models
  * @property integer $id
- * @property string $product_name
- * @property string|null $product_code
- * @property integer $product_qty
+ * @property string $name
  */
 
-class Product extends Model
+class Material extends Model
 {
     use HasFactory;
 
     protected $fillable = [
         'product_name',
-        'product_code',
-        'product_qty',
     ];
 
     protected $hidden = [self::CREATED_AT, self::UPDATED_AT];
 
     protected $casts = [
         'product_name' => 'string',
-        'product_code' => 'string',
-        'product_qty' => 'integer',
     ];
 }
